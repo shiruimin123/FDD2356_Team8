98a535476d7a5d422714e96e3129aad90b16d4e1
Ruimin Shi <ruimins@kth.se>
2025-04-21 23:50:24 +0200

    openmp+cython

--- Project/cPython_v3/cythonfn.pyx
+++ Project/cPython_v3/cythonfn.pyx
@@ -4,7 +4,8 @@ cimport cython
 from libc.math cimport sqrt, fabs
 from cython.parallel import prange
 
-
+@cython.boundscheck(False)
+@cython.wraparound(False)
 def extrapolateInSpaceToFace_cython(double[:, :] f, 
                                   double[:, :] f_dx, 
                                   double[:, :] f_dy, 
@@ -21,22 +22,26 @@ def extrapolateInSpaceToFace_cython(double[:, :] f,
     cdef np.ndarray[np.double_t, ndim=2] f_YR = np.empty((nx, ny), dtype=np.double)
     
     # Calculate face values
-    for i in prange(nx, nogil=True):
-        for j in prange(ny):
-            f_XR[i,j] = f[i,j] + f_dx[i,j] * dx/2
-            f_XL[i,j] = f[i,j] - f_dx[i,j] * dx/2
-            f_YR[i,j] = f[i,j] + f_dy[i,j] * dx/2
-            f_YL[i,j] = f[i,j] - f_dy[i,j] * dx/2
+    cdef int num_threads
+
+    with nogil:
+        for i in prange(nx, schedule='static'):
+            for j in prange(ny):
+                f_XR[i,j] = f[i,j] + f_dx[i,j] * dx/2
+                f_XL[i,j] = f[i,j] - f_dx[i,j] * dx/2
+                f_YR[i,j] = f[i,j] + f_dy[i,j] * dx/2
+                f_YL[i,j] = f[i,j] - f_dy[i,j] * dx/2
     
     # Roll arrays for shifted values (equivalent to np.roll)
     # For f_XL (roll right along axis 0)
     cdef np.ndarray[np.double_t, ndim=2] f_XL_rolled = np.empty((nx, ny), dtype=np.double)
-    for i in range(nx):
-        for j in range(ny):
-            if i == nx-1:
-                f_XL_rolled[0,j] = f_XL[i,j]  # Wrap around
-            else:
-                f_XL_rolled[i+1,j] = f_XL[i,j]
+    with nogil:
+        for i in prange(nx, schedule='static'):
+            for j in range(ny):
+                if i == nx-1:
+                    f_XL_rolled[0,j] = f_XL[i,j]  # Wrap around
+                else:
+                    f_XL_rolled[i+1,j] = f_XL[i,j]
     
     # For f_YL (roll right along axis 1)
     cdef np.ndarray[np.double_t, ndim=2] f_YL_rolled = np.empty((nx, ny), dtype=np.double)
@@ -70,35 +75,36 @@ def getFlux(double[:, :] rho_L, double[:, :] rho_R,
     cdef double en_L, en_R, rho_star, momx_star, momy_star, en_star, P_star
     cdef double C_L, C_R, C
     
-    for i in prange(nx,nogil=True):
-        for j in prange(ny):
-            # left and right energies
-            en_L = P_L[i,j]/(gamma-1) + 0.5*rho_L[i,j] * (vx_L[i,j]**2 + vy_L[i,j]**2)
-            en_R = P_R[i,j]/(gamma-1) + 0.5*rho_R[i,j] * (vx_R[i,j]**2 + vy_R[i,j]**2)
+    with nogil:
+        for i in prange(nx, schedule='static'):
+            for j in prange(ny):
+                # left and right energies
+                en_L = P_L[i,j]/(gamma-1) + 0.5*rho_L[i,j] * (vx_L[i,j]**2 + vy_L[i,j]**2)
+                en_R = P_R[i,j]/(gamma-1) + 0.5*rho_R[i,j] * (vx_R[i,j]**2 + vy_R[i,j]**2)
 
-            # compute star (averaged) states
-            rho_star  = 0.5*(rho_L[i,j] + rho_R[i,j])
-            momx_star = 0.5*(rho_L[i,j] * vx_L[i,j] + rho_R[i,j] * vx_R[i,j])
-            momy_star = 0.5*(rho_L[i,j] * vy_L[i,j] + rho_R[i,j] * vy_R[i,j])
-            en_star   = 0.5*(en_L + en_R)
-            
-            P_star = (gamma-1)*(en_star-0.5*(momx_star**2+momy_star**2)/rho_star)
-            
-            # compute fluxes (local Lax-Friedrichs/Rusanov)
-            flux_Mass[i,j] = momx_star
-            flux_Momx[i,j] = momx_star**2/rho_star + P_star
-            flux_Momy[i,j] = momx_star * momy_star/rho_star
-            flux_Energy[i,j] = (en_star+P_star) * momx_star/rho_star
-            
-            # find wavespeeds
-            C_L = sqrt(gamma*P_L[i,j]/rho_L[i,j]) + fabs(vx_L[i,j])
-            C_R = sqrt(gamma*P_R[i,j]/rho_R[i,j]) + fabs(vx_R[i,j])
-            C = C_L if C_L > C_R else C_R
-            
-            # add stabilizing diffusive term
-            flux_Mass[i,j] -= C * 0.5 * (rho_L[i,j] - rho_R[i,j])
-            flux_Momx[i,j] -= C * 0.5 * (rho_L[i,j] * vx_L[i,j] - rho_R[i,j] * vx_R[i,j])
-            flux_Momy[i,j] -= C * 0.5 * (rho_L[i,j] * vy_L[i,j] - rho_R[i,j] * vy_R[i,j])
-            flux_Energy[i,j] -= C * 0.5 * (en_L - en_R)
+                # compute star (averaged) states
+                rho_star  = 0.5*(rho_L[i,j] + rho_R[i,j])
+                momx_star = 0.5*(rho_L[i,j] * vx_L[i,j] + rho_R[i,j] * vx_R[i,j])
+                momy_star = 0.5*(rho_L[i,j] * vy_L[i,j] + rho_R[i,j] * vy_R[i,j])
+                en_star   = 0.5*(en_L + en_R)
+                
+                P_star = (gamma-1)*(en_star-0.5*(momx_star**2+momy_star**2)/rho_star)
+                
+                # compute fluxes (local Lax-Friedrichs/Rusanov)
+                flux_Mass[i,j] = momx_star
+                flux_Momx[i,j] = momx_star**2/rho_star + P_star
+                flux_Momy[i,j] = momx_star * momy_star/rho_star
+                flux_Energy[i,j] = (en_star+P_star) * momx_star/rho_star
+                
+                # find wavespeeds
+                C_L = sqrt(gamma*P_L[i,j]/rho_L[i,j]) + fabs(vx_L[i,j])
+                C_R = sqrt(gamma*P_R[i,j]/rho_R[i,j]) + fabs(vx_R[i,j])
+                C = C_L if C_L > C_R else C_R
+                
+                # add stabilizing diffusive term
+                flux_Mass[i,j] -= C * 0.5 * (rho_L[i,j] - rho_R[i,j])
+                flux_Momx[i,j] -= C * 0.5 * (rho_L[i,j] * vx_L[i,j] - rho_R[i,j] * vx_R[i,j])
+                flux_Momy[i,j] -= C * 0.5 * (rho_L[i,j] * vy_L[i,j] - rho_R[i,j] * vy_R[i,j])
+                flux_Energy[i,j] -= C * 0.5 * (en_L - en_R)
 
     return flux_Mass, flux_Momx, flux_Momy, flux_Energy
